8fa8ff1ff67b7525eaa6a76d5752d14d6f41c6f9
Vladislav <vtiterin@users.noreply.github.com>
2026-01-03 13:44:16 +0200

    3
    3

--- Source/TileCoverageCSShaders/Private/TileCoverageShader.cpp
+++ Source/TileCoverageCSShaders/Private/TileCoverageShader.cpp
@@ -1,24 +1,8 @@
 ﻿#include "TileCoverageShader.h"
 
-static void ForceLinkTileCoverageCSShader()
-{
-}
-
 IMPLEMENT_GLOBAL_SHADER(
 	FTileCoverageCS,
 	"/Plugin/TileCoverageCS/TileCoverageCS.usf",
 	"MainCS",
 	SF_Compute
 );
-
-// гарантований виклик при завантаженні TU
-struct FTileCoverageCSShaderLinker
-{
-	FTileCoverageCSShaderLinker()
-	{
-		ForceLinkTileCoverageCSShader();
-	}
-};
-
-// глобальний обʼєкт → static init
-static FTileCoverageCSShaderLinker GTileCoverageCSShaderLinker;
